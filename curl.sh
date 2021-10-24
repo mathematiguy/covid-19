016af081ba105600386765d8f6b9c1089e128db2
Caleb Moses <caleb@dragonfly.co.nz>
2021-10-25 04:26:41 +1300

    Delete curl.sh

--- curl.sh
+++
@@ -1,3 +0,0 @@
-curl -v -X GET "https://api.stats.govt.nz/opendata/v1/Covid-19Indicators/Observations?$filter=(ResourceID%20eq%20'CPCOV2')" \
--H "Cache-Control: no-cache" \
--H "Ocp-Apim-Subscription-Key: ''"
